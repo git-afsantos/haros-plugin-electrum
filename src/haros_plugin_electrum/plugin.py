813cb7f1cfc0c88df0670b63520a2b484a2ee198
git-afsantos <contact.andre.santos@gmail.com>
2021-03-05 10:54:39 +0000

    added basic structure

--- src/haros_plugin_electrum/plugin.py
+++ src/haros_plugin_electrum/plugin.py
@@ -8,6 +8,11 @@
 ###############################################################################
 
 from __future__ import unicode_literals
+import os
+
+from .analyzer import model_check
+from .electrum2haros import parse_output, render_result
+from .haros2electrum import config2model, render_model
 
 
 ################################################################################
@@ -20,11 +25,22 @@ def configuration_analysis(iface, config):
         return
     settings = config.user_attributes.get('haros_plugin_electrum', {})
     try:
-        _validate_settings(iface, settings)
+        _validate_settings(settings)
     except PluginSettingsError as e:
         iface.log_error(e.message)
+    jar_path = _get_jar_path(settings)
+    try:
+        model = config2model(config)
+        ele_src = render_model(model)
+        output = model_check(jar_path, ele_src)
+        result = parse_output(output, config)
+        html = render_result(result)
+        iface.report_runtime_violation('counterexample', html, result.resources)
+    except Exception as e:
+        iface.log_error(e.message)
 
-def _validate_settings(iface, settings):
+
+def _validate_settings(settings):
     jar = settings.get('jar')
     if not jar:
         raise PluginSettingsError.missing_jar()
@@ -32,6 +48,16 @@ def _validate_settings(iface, settings):
         raise PluginSettingsError.jar_not_dict()
     if not 'path' in jar and not 'env' in jar:
         raise PluginSettingsError.missing_jar_path()
+    if 'env' in jar and not os.environ.get(jar['env']):
+        raise PluginSettingsError.missing_env_var(jar['env'])
+
+def _get_jar_path(settings):
+    jar = settings['jar']
+    path = jar.get('path')
+    if path is None:
+        env_var = jar['env']
+        path = os.environ[env_var]
+    return path
 
 
 ################################################################################
@@ -50,3 +76,7 @@ class PluginSettingsError(Exception):
     @classmethod
     def missing_jar_path(cls):
         return cls('missing .jar path; expected one of `jar:path` or `jar:env`')
+
+    @classmethod
+    def missing_env_var(cls, var):
+        return cls("missing '{}' env. variable with .jar path".format(var))
